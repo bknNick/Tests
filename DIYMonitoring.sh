6425a79cd95773acc4022828dde6ff8e0a5b3565
Nick <45410394+bknNick@users.noreply.github.com>
2020-06-04 16:21:48 +0300

    DYIMon

--- DIYMonitoring.sh
+++ DIYMonitoring.sh
@@ -13,6 +13,10 @@
 ## If no output is returned, then all is working as intended.                                                                                              ##
 #############################################################################################################################################################
 
+#############################################################################################################################################################
+##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!PLEASE HAVE THIS BACKED UP BEFORE CHANGING IT, OR I WILL CRY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##
+#############################################################################################################################################################
+
 ##THRESHOLDS IN PERCENTAGES, EDIT HERE!:
 DiscUsageThreshold=70
 InodeUsageThreshold=70
@@ -23,39 +27,34 @@ FSmonitoring(){
 
         for fsusage in $(df -h | awk '{print $5}' | grep -vi "use" | cut -d "%" -f 1); do
                 if (( $fsusage >= $DiscUsageThreshold )); then
-                        echo ""
-                        echo "FS issue detected in the following FS', please review! :"
-                        echo "$(df -h | grep "$fsusage"%)"
-                        echo ""
-                        echo "The largest files can be found below:"
-                        echo ""
+                        printf "\nFS issue detected in the following FS', please review!\n"
+                        echo $(df -h | grep "$fsusage")
+                        printf "\nThe largest files can be found below:\n"
+                        printf "\n"
                         for DiscUsageFile in $(find / -type f -size +1000000c -exec du -m {} \; 2>/dev/null | sort -n -k 1 | tail -20 | awk {'print ($2)'});
                                 do ls -l --block-size=M -a $DiscUsageFile
                         done
-                        echo ""
+                        printf "\n"
                 fi
         done
 
 ######## INODE usage monitoring - same as the above, but prints out a list with the directories using the most inodes.
         for iusage in $(df -i | awk '{print $5}' | grep -vi "use" | cut -d "%" -f 1); do
                 if (( $iusage >= $InodeUsageThreshold )); then
-                        echo ""
-                        echo "Inode issue detected in the following FS', please review! :"
+                        printf "\nInode issue detected in the following FS', please review!\n"
                         echo "$(df -i | grep "$iusage"%)"
-                        echo ""
-                        echo "Directories with the most inodes on the system can be found below:"
+                        printf "\nDirectories with the most inodes on the system can be found below:\n"
                         find / -xdev -printf '%h\n' | sort | uniq -c | sort -k 1 -n | tail -20
-                        echo ""
+                        printf "\n"
                 fi
         done
 
 ####### Read only FS check
         local RO=$(grep "ro," /proc/mounts| grep -vi tmpfs)
         if [[ $RO ]]; then
-                echo ""
-                echo "Read only filesystem detected!"
-                echo $RO
-                echo ""
+                printf "\nRead only filesystem detected!\n"
+                echo "$RO"
+                printf "\n"
         fi
 }
 
@@ -66,11 +65,12 @@ MEMmonitoring(){
 
         local FreeMem=$(free -m | grep -i "mem" | awk '{print ($4 * 100 / $2)}' | cut -d "." -f 1)
         if (( $FreeMem <= $FreeMemThreshold )); then
-                echo ""
+                printf "\n"
                 echo "Memory issue detected! Free memory is only $FreeMem%!"
-                echo ""
-                echo "The highest memory consuming processes can be found below:"
+                printf "\n"
+                printf "The highest memory consuming processes can be found below:\n"
                 ps -eo pid,ppid,stat,vsz,rss,comm --sort=rss | tail -20
+                printf "\n"
         fi
 }
 MEMmonitoring
@@ -84,20 +84,16 @@ SvcsMonitoring(){
         local ApacheStart="/bin/systemctl start httpd"
         local ApacheStatus="/bin/systemctl status httpd"
         if [[ $ApacheDown ]]; then
-                echo ""
-                echo "HTTPD service was stopped. Trying to start it..."
+                printf "\nHTTPD service was stopped. Trying to start it...\n"
                 $ApacheStart > /dev/null
                 wait
                 local ApacheDown=$(/bin/systemctl status httpd | grep -i "Active: inactive (dead)")
                 if [[ $ApacheDown ]]; then
-                        echo ""
-                        echo "HTTPD issue detected. Please review!"
-                        echo ""
+                        printf "\nHTTPD issue detected. Please review!\n"
                         $ApacheStatus
-                        echo ""
                 elif [[ ! $ApacheDown ]]; then
-                        echo "Httpd successfully started."
-                        echo ""
+                        printf "HTTPD successfully started.\n"
+                        printf "\n"
                 fi
         fi
 
@@ -106,20 +102,16 @@ SvcsMonitoring(){
         local MySQLStart="/bin/systemctl start mysqld"
         local MySQLStatus="/bin/systemctl status mysqld"
         if [[ $MySQLDown ]]; then
-                echo ""
-                echo "MySQL service was stopped. Trying to start it..."
+                printf "MySQL service was stopped. Trying to start it...\n"
                 $MySQLStart > /dev/null
                 wait
                 local MySQLDown=$(/bin/systemctl status mysqld | grep -i "Active: inactive (dead)")
                 if [[ $MySQLDown ]]; then
-                        echo ""
-                        echo "MySQL issue detected. Please review!"
-                        echo ""
+                        printf "\nMySQL issue detected. Please review!\n"
                         $MySQLStatus
-                        echo ""
                 elif [[ ! $MySQLDown ]]; then
-                        echo "MySQL successfully started."
-                        echo ""
+                        printf "MySQL successfully started.\n"
+                        printf "\n"
                 fi
         fi
 
