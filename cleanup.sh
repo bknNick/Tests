8086284a74ee9c4e1b0acb37a5ac15dfb1787588
Nick <45410394+bknNick@users.noreply.github.com>
2021-04-24 02:19:56 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#uninstall all from initialization script.
-
+#uninstall all from server
 read -p "input target server ip: " IPADDR
-ssh $IPADDR 'yum remove fail2ban -y && yum remove mysql -y && yum remove httpd -y && yum remove zip -y && rm -rf /root/own* && rm -rf /var/www/html/wordpress || echo "breaks on removing wordpress and ends." && rm -rf /var/www/html/*  || echo "breaks on deleting * from html and ends here." && rm -rf /var/www/html/latest.zip && yum remove php -y && yum remove perl -y && yum remove php-mysqlnd -y && yum clean all'
+ssh $IPADDR 'yum remove fail2ban -y && yum remove mysql -y && yum remove httpd -y && yum remove zip -y && rm -rf /root/own* && rm -rf /var/www/html/wordpress || echo "breaks on removing wordpress and ends." && rm -rf /var/www/html/*  || echo "breaks on deleting * from html and ends here." && rm -rf /var/www/html/latest.zip && yum remove php -y && yum remove perl -y && yum remove php-mysqlnd -y && yum clean all && yum remove php-json -y'
